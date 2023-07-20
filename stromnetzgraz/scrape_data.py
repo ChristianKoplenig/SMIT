94b32648108eb58b6ebee8755345184819fd4b15
ckoplenig <tmpck@gmx.net>
2023-07-20 12:12:05 +0200

    scrape_data - access to download area works

--- stromnetzgraz/scrape_data.py
+++ stromnetzgraz/scrape_data.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import login_details
+import time
 
 username = login_details.username
 password = login_details.password
@@ -8,7 +9,12 @@ password = login_details.password
 driver = webdriver.Firefox()
 driver.get('https://webportal.stromnetz-graz.at/login')
 
+# Login
 driver.find_element(By.NAME, "email").send_keys(username)
+driver.find_element(By.NAME, "password").send_keys(password)
+driver.find_element(By.XPATH,"/html/body/div/app-root/main/div/app-login/div[2]/div[1]/form/div[3]/button").click()
+# wait for home webpage loading
+time.sleep(5)
 
-# driver.get("https://www.linkedin.com/uas/login?")
-# driver.maximize_window()
+# click for analysis webpage
+driver.find_element(By.XPATH,"/html/body/div/app-root/main/div/app-dashboard/div[2]/div/div[1]/div[1]/div").click()
