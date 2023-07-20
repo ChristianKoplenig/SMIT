95180a0f9b17dbc359352adce1f1138967cc82c0
ckoplenig <tmpck@gmx.net>
2023-07-20 20:49:56 +0200

    scrape_data.py - code cleaning

--- stromnetzgraz/scrape_data.py
+++ stromnetzgraz/scrape_data.py
@@ -1,10 +1,11 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import login_details
-import time
 
+# import user credentials from file
 username = login_details.username
 password = login_details.password
 
