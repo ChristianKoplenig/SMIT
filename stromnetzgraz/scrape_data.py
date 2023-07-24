7487080c9169e19125a73ffa1fc677a461a62b6d
ckoplenig <tmpck@gmx.net>
2023-07-24 14:28:18 +0200

    scrape - basic access functions implemented, download tagstrom works, dates hardcoded

--- stromnetzgraz/scrape_data.py
+++ stromnetzgraz/scrape_data.py
@@ -6,53 +6,28 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.firefox.options import Options
 import login_details
 
-# import user credentials from file
-username = login_details.username
+# user input
+folder = "/home/c/Downloads/csv"    # download folder for csv files
+username = login_details.username   # import user credentials
 password = login_details.password
 
-##### set firefox profile #####
-profile = Options()
-profile.set_preference("browser.download.folderList", 2)
-profile.set_preference("browser.download.manager.showWhenStarting", False)
-profile.set_preference("browser.download.dir", "/home/c/Downloads/csv")
+def ff_options(dl_folder):
+    ''' 
+    set options for firefox webdriver 
+    dl_folder: target download directory
+    '''
+    profile = Options()
+    profile.set_preference("browser.download.folderList", 2)
+    profile.set_preference("browser.download.manager.showWhenStarting", False)
+    profile.set_preference("browser.download.dir", dl_folder)
+    return profile
 
-##### open firefox instance #####
-driver = webdriver.Firefox(options=profile)
-driver.get('https://webportal.stromnetz-graz.at/login')
-driver.maximize_window()
-
-##### login #####
-driver.find_element(By.NAME, "email").send_keys(username)
-driver.find_element(By.NAME, "password").send_keys(password)
-driver.find_element(By.XPATH,"/html/body/div/app-root/main/div/app-login/div[2]/div[1]/form/div[3]/button").click()
-# wait for loading page
-time.sleep(2)
-# click for analysis webpage
-driver.find_element(By.XPATH,"/html/body/div/app-root/main/div/app-dashboard/div[2]/div/div[1]/div[1]/div").click()
-# wait for loading page
-time.sleep(2)
-
-##### define buttons #####
-tageswerte_btn = driver.find_element(By.XPATH,"/html/body/div/app-root/main/div/app-overview/div/app-period-selector/div[1]/div/div[5]/div")
-units_btn = driver.find_element(By.XPATH,"/html/body/div/app-root/main/div/app-overview/div/div[2]/div[3]/app-unit-selector/div/div[2]")
-tageswerte_btn.click()
-units_btn.click()
-start_picker = driver.find_element(By.ID, "fromDayOverviewDate")
-end_picker = driver.find_element(By.ID, 'toDayOverviewDate')
-confirm_btn = driver.find_element(By.XPATH, '/html/body/div/app-root/main/div/app-overview/div/app-period-selector/div[2]/div/div/div/div[2]/div[2]/div[2]/button')
-werte_btn = driver.find_element(By.XPATH, '/html/body/div/app-root/main/div/app-overview/div/div[2]/div[1]/div/div[1]')
-download_btn = driver.find_element(By.XPATH, '/html/body/div/app-root/main/div/app-overview/reports-nav/app-header-nav/nav/div/div/div/div/div[2]/div/div[3]/div/div[2]/span')
-
-##### setup for export #####
-actions = ActionChains(driver)
-start_picker.click()
-
-# date selection
 def date_selector(day, month, year):
     '''
-    set start date for csv file
+    set date for csv file
     input dateformat 'dd', 'mm', 'yyyy'
     '''
+    actions = ActionChains(driver)
     actions.send_keys(month) #month
     actions.send_keys(day) #day
     actions.send_keys(year)
@@ -60,9 +35,49 @@ def date_selector(day, month, year):
     actions.send_keys(Keys.TAB)
     actions.perform()
 
+def stromnetz_setup(dl_folder):
+    '''
+    login to stromnetz graz webportal and setup data page
+    '''
+    global driver
+    driver = webdriver.Firefox(options=ff_options(dl_folder))
+    driver.get('https://webportal.stromnetz-graz.at/login')
+    driver.maximize_window()
 
-date_selector('01', '02', '2023') # start date
-date_selector('03', '04', '2023') # end date
-confirm_btn.click()
-werte_btn.click()
-download_btn.click()
+    ##### login #####
+    driver.find_element(By.NAME, "email").send_keys(username)
+    driver.find_element(By.NAME, "password").send_keys(password)
+    driver.find_element(By.XPATH,"/html/body/div/app-root/main/div/app-login/div[2]/div[1]/form/div[3]/button").click()
+    time.sleep(2)
+    # go to data page
+    driver.find_element(By.XPATH,"/html/body/div/app-root/main/div/app-dashboard/div[2]/div/div[1]/div[1]/div").click()
+    time.sleep(2)
+    # set units to [Wh]
+    units_btn = driver.find_element(By.XPATH,"/html/body/div/app-root/main/div/app-overview/div/div[2]/div[3]/app-unit-selector/div/div[2]")
+    units_btn.click()
+
+def stromnetz_fillTageswerte():
+    '''
+    for daily average computation
+    set start and end dates
+    '''
+    tageswerte_btn = driver.find_element(By.XPATH,"/html/body/div/app-root/main/div/app-overview/div/app-period-selector/div[1]/div/div[5]/div")
+    tageswerte_btn.click()
+    start_picker = driver.find_element(By.ID, "fromDayOverviewDate")
+    start_picker.click()
+    date_selector('01', '02', '2023') # start date
+    date_selector('03', '04', '2023') # end date
+    time.sleep(1)
+    
+def stromnetz_download():
+    '''
+    confirm dates and start download
+    '''
+    confirm_btn = driver.find_element(By.XPATH, '/html/body/div/app-root/main/div/app-overview/div/app-period-selector/div[2]/div/div/div/div[2]/div[2]/div[2]/button')
+    download_btn = driver.find_element(By.XPATH, '/html/body/div/app-root/main/div/app-overview/reports-nav/app-header-nav/nav/div/div/div/div/div[2]/div/div[3]/div/div[2]/span')
+    confirm_btn.click()
+    download_btn.click()
+    
+stromnetz_setup(folder)
+stromnetz_fillTageswerte()
+stromnetz_download()
