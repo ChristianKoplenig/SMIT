54d1b2735fdcb20a275bc33d60ce4b52e09112a8
ckoplenig <tmpck@gmx.net>
2023-07-25 10:12:46 +0200

    scrape - moved static variables to user_data.py

---
+++ stromnetzgraz/user_data.py
@@ -0,0 +1,8 @@
+# Login credentials
+login_url = 'https://webportal.stromnetz-graz.at/login'
+username = 'ch.koplenig@posteo.net'
+password = 'Gwhwk12.'
+
+# Folder structure
+csv_dlFolder = '/home/c/Downloads/csv'  # Download folder for csv files
+persist_dates = './dates.pkl'    # Filename for dates persistence
