7e8627f756a5dddd76ae60dec2d7d829a3465e53
ckoplenig <tmpck@gmx.net>
2023-07-25 09:34:14 +0200

    scrape - date updater, switched to dict type for persistence variable

--- stromnetzgraz/date_updater.py
+++ stromnetzgraz/date_updater.py
@@ -2,49 +2,50 @@ from datetime import date, timedelta
 import pickle
 import os.path
 
-start_date = '01-01-2023'
-end_date = '02-01-2023'
+dates = dict()
+dates['start'] = '01-01-2023'
+dates['end'] = '02-01-2023'
 
 def date_persist():
     '''
     get yesterdays date and persist it for future start date in date_updater function
     '''
-    path = './scrape_log.pkl'
+    path = './dates.pkl'
     if not os.path.isfile(path):
-        with open('scrape_log.pkl', 'wb') as pk:
-            scrape_log = []
-            scrape_log.append((date.today() - timedelta(days=1)).strftime('%d-%m-%Y'))
-            pickle.dump(scrape_log, pk)
+        with open('dates.pkl', 'wb') as pk:
+            global dates
+            dates['last_scrape'] = (date.today() - timedelta(days=1)).strftime('%d-%m-%Y')
+            pickle.dump(dates, pk)
         pk.close()
     else:
-        with open('scrape_log.pkl', 'rb') as pk:    # append today to logfile
-            scrape_log = pickle.load(pk)
-            if not (scrape_log[-1] == (date.today() - timedelta(days=1)).strftime('%d-%m-%Y')): # check if yesterdays date already in log
-                scrape_log.append((date.today() - timedelta(days=1)).strftime('%d-%m-%Y'))
-                print('appended yesterdays date to scrape_log: ' + str(scrape_log))
+        with open('dates.pkl', 'rb') as pk:    # append today to logfile
+            dates = pickle.load(pk)
+            if not (dates['last_scrape'] == (date.today() - timedelta(days=1)).strftime('%d-%m-%Y')): # check if yesterdays date already in log
+                dates['last_scrape'] = (date.today() - timedelta(days=1)).strftime('%d-%m-%Y')
+                print('appended yesterdays date to dates: ' + str(dates['last_scrape']))
             else:
                 print('yesterdays date already persisted')
         pk.close()
-        with open('scrape_log.pkl', 'wb') as pk:    # save logfile
-            pickle.dump(scrape_log, pk)
+        with open('dates.pkl', 'wb') as pk:    # save logfile
+            pickle.dump(dates, pk)
         pk.close()
-    return scrape_log
+    return dates
 
 def date_updater():
     '''
-    update start/end date for automated date setup
+    update start/end date for autofill
     '''
-    global start_date
-    global end_date
-    start_date= date_persist()[-1]
-    end_date = (date.today() - timedelta(days=1)).strftime('%d-%m-%Y')
-    return start_date, end_date
+    global dates
+    dates['start'] = date_persist()['last_scrape']
+    dates['end'] = (date.today() - timedelta(days=1)).strftime('%d-%m-%Y')
+    return dates
 
-print('start before: ' + start_date)
-print('end before: ' + end_date)
-print('type start before: ' + str(type(start_date)))
+print('start before: ' + dates['start'])
+print('end before: ' + dates['end'])
+print('type start before: ' + str(type(dates['start'])))
 date_updater()
 print('-'*10)
-print('start after: ' + start_date)
-print('end after: ' + end_date)
-print('type start after: ' + str(type(end_date)))
+print('start after: ' + dates['start'])
+print('end after: ' + dates['end'])
+print('type start after: ' + str(type(dates['end'])))
+print(dates)
