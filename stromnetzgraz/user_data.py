dcf78006fa7174f75f71cd5d952634a7f6b263bf
ckoplenig <tmpck@gmx.net>
2023-07-26 12:59:47 +0200

    scrape - implemented firefox headless mode

--- stromnetzgraz/user_data.py
+++ stromnetzgraz/user_data.py
@@ -3,6 +3,9 @@ login_url = 'https://webportal.stromnetz-graz.at/login'
 username = 'ch.koplenig@posteo.net'
 password = 'Gwhwk12.'
 
+# Firefox options
+headless_mode = True # Run Firefox in headless mode, type: boolean
+
 # Folder structure
 csv_dlFolder = '/home/c/Downloads/csv'  # Download folder for csv files
 persist_dates = './dates.pkl'    # Filename for dates persistence
