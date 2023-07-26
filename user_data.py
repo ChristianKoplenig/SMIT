7f7653eabdffa8e9bc5aa3790bc791ad4151a4f7
ckoplenig <tmpck@gmx.net>
2023-07-26 14:49:51 +0200

    changed root folder

---
+++ user_data.py
@@ -0,0 +1,15 @@
+# Login credentials
+login_url = 'https://webportal.stromnetz-graz.at/login'
+username = ''
+password = ''
+
+# Firefox options
+headless_mode = True                    # Run Firefox in headless mode, type: boolean
+
+# Folder structure
+csv_dlFolder = '/home/c/Downloads/csv'  # Download folder for csv files
+persist_dates = './dates.pkl'           # Filename for dates persistence
+webdriver_logFolder = './Log/geckodriver.log'
+
+# Csv File Options
+csv_startDate = '01-01-2023'            # Start date for initial run
