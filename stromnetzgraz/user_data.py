1eb0df2c8f204c48a2bf144bb2bc8a3c98bc4a98
ckoplenig <tmpck@gmx.net>
2023-07-26 13:43:23 +0200

    user_data - code cleaning

--- stromnetzgraz/user_data.py
+++ stromnetzgraz/user_data.py
@@ -4,11 +4,11 @@ username = 'ch.koplenig@posteo.net'
 password = 'Gwhwk12.'
 
 # Firefox options
-headless_mode = True # Run Firefox in headless mode, type: boolean
+headless_mode = True                    # Run Firefox in headless mode, type: boolean
 
 # Folder structure
 csv_dlFolder = '/home/c/Downloads/csv'  # Download folder for csv files
-persist_dates = './dates.pkl'    # Filename for dates persistence
+persist_dates = './dates.pkl'           # Filename for dates persistence
 
 # Csv File Options
-csv_startDate = '01-01-2023'
+csv_startDate = '01-01-2023'            # Start date for initial run
