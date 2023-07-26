7ccf8d936dfe9bf70a3cf64bdab87703fb09aa7b
ckoplenig <tmpck@gmx.net>
2023-07-26 15:57:00 +0200

    set folders in user_data implemented

--- user_data.py
+++ user_data.py
@@ -1,15 +1,15 @@
 # Login credentials
 login_url = 'https://webportal.stromnetz-graz.at/login'
-username = ''
-password = ''
+username = 'ch.koplenig@posteo.net'
+password = 'Gwhwk12.'
 
 # Firefox options
-headless_mode = True                    # Run Firefox in headless mode, type: boolean
+headless_mode = False                    # Run Firefox in headless mode, type: boolean
 
 # Folder structure
-csv_dlFolder = '/home/c/Downloads/csv'  # Download folder for csv files
-persist_dates = './dates.pkl'           # Filename for dates persistence
-webdriver_logFolder = './Log/geckodriver.log'
+csv_dlFolder = './csv_raw'                      # Download folder for csv files
+persist_dates = './log/dates.pkl'               # Filename for dates persistence
+webdriver_logFolder = './log/geckodriver.log'   # Log folder for webdriver
 
 # Csv File Options
 csv_startDate = '01-01-2023'            # Start date for initial run
