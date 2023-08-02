c530cab0559647e3099159ee28a78450c7cd8751
ckoplenig <tmpck@gmx.net>
2023-08-02 14:16:56 +0200

    create all data df's via function implemented

--- modules/filehandling.py
+++ modules/filehandling.py
@@ -3,15 +3,18 @@ Tools for manipulating files on operating system level
 '''
 import datetime as dt
 import pathlib as pl
+import pandas as pd
 # Custom imports
 from modules import dynamicclass
+from modules import filepersistence
+from modules.scrapedata import get_daysum_files
 
 # create user class
 User = dynamicclass.create_user()
 
 def pathlib_move(src,dest,appendix):
     '''
-    use pathlib to move file
+    use pathlib to move file, rename file: 'today_appendix.csv'
     src, dest: filetype= pathlib path
     appendix: type string, append to filename
     '''
@@ -43,3 +46,49 @@ def move_files(meter_number):
             #filter for input files
             if meter_number in str(filename):
                 pathlib_move(filename, workdir, meter_number)
+                
+def create_dataframe(workdir, metertype):
+    '''
+    Create dataframe for data analysis
+    workdir: path to csv files
+    metertype: either day or night meter
+    '''
+    path = pl.Path(workdir)
+    df_return = pd.DataFrame()
+    filelist = []
+
+    for filename in path.glob('*.csv'):
+        if (str(metertype) in filename.name):
+            filelist.append(filename)
+            df_return = pd.concat((pd.read_csv(r,
+                                              sep=';',
+                                              decimal=',',
+                                              header=0,
+                                              parse_dates=['date'],
+                                              converters={'date': lambda t: dt.datetime.strptime(t, '%Y-%m-%dT%H:%M:%S.%f%z').date()},
+                                              names=['date', 'zaehlerstand', '1', '2', 'verbrauch', '3', '4'],
+                                              usecols=lambda x: x in ['date', 'zaehlerstand', 'verbrauch'],
+                                            )
+                                for r in filelist))
+
+            df_return['zaehlerstand'] = df_return['zaehlerstand'].astype(float)
+            df_return['verbrauch'] = df_return['verbrauch'].astype(float)
+            df_return.sort_values(by='date', inplace=True)
+            df_return.reset_index(drop=True, inplace=True)
+            df_return.drop_duplicates(subset='date', keep='first', inplace=True)
+    return df_return
+
+def scrapandmove():
+    '''
+    Download files from stromnetzgraz and move to work directory
+    '''
+    filepersistence.initialize_dates_log()
+    dates = filepersistence.create_dates_var()
+   
+    if not dates['start'] == dt.date.today().strftime('%d-%m-%Y'):                 # scrape just once a day
+    
+        get_daysum_files(User.headless_mode)
+        move_files(User.day_meter)
+        move_files(User.night_meter)
+    else:
+        print('Most recent data already downloaded')
--- scrapeandmove.py
+++
@@ -1,9 +0,0 @@
-from modules.scrapedata import get_dn_daily
-from modules.filehandling import move_files
-import modules.dynamicclass as dynamicclass
-
-User = dynamicclass.create_user()
-
-get_dn_daily(User.headless_mode)
-move_files(User.day_meter)
-move_files(User.night_meter)
