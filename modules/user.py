4f37f37f2fa5ae4e0a1ea86bcc5cf737c224e039
mike-landl <13545527+mike-landl@users.noreply.github.com>
2023-08-05 14:58:53 +0200

    wrap dictionary in user class

--- modules/user.py
+++ modules/user.py
@@ -4,25 +4,56 @@ if sys.version_info < (3, 11):
 else:
     import tomlib
 
-def create_user(file_path : str = './user_data.toml') -> dict:
+class user():
     """
-    Reads a toml config file from *file_path* and returns it as a dictionary.
+    A class that holds all the userdefined data and settings.
 
-    Parameters
+    Attributes
     ----------
-    file_path : str
-        path to user config file
+    Attributes are infered during run time from the TOML config file.
 
-    Returns
+    Methods
     -------
-    user : dict
-        holds key-value pairs of user data
+    None
     """
-    with open(file_path, 'rb') as config_file:
-        user = tomlib.load(config_file)
-    return user
 
+    def __init__(self, file_path : str = './user_data.toml'):
+        """
+        The constructor reads a TOML config file from *file_path*
+        and loops through the dictionary in order to set the attributes
+        of the class to self.key = value.
+
+        Parameters
+        ----------
+        file_path : str, optional
+            path to user config file
+        """
+
+        # load file
+        with open(file_path, 'rb') as file:
+            data = tomlib.load(file)
+
+        # loop through all key value pairs of the config file
+        # and set them as attributes of the class
+        for key, value in data.items():
+            setattr(self, key, value)
+
+    def __repr__(self):
+        """
+        This special function gets called if you use print on a class instance.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        str
+            Returns a string representation of the class.
+        """
+        return self.username
 
 if __name__ == '__main__':
-    user = create_user('../user_data.toml')
-    print(user['username'])
+    User = user('../user_data.toml')
+    print(User.username)
+    print(User)
