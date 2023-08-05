7b69c080f41f0acba75f9974e7e3ee33fb48b1d2
mike-landl <13545527+mike-landl@users.noreply.github.com>
2023-08-05 14:32:19 +0200

    Revert "changed item retrieval from class attributes to dictionary"
    This reverts commit f3e668c638c640b0d6d2125a0463247d74f751b7.

--- modules/filepersistence.py
+++ modules/filepersistence.py
@@ -10,14 +10,14 @@ import modules.user
 User = modules.user.create_user()
 
 def initialize_dates_log():
-    '''
+    ''' 
     create log file for managing scrape dates
     If no persisted date exists, create dict for dates and use start date from User
     '''
-    if not os.path.isfile(User['persist_dates']):
-        with open(User['persist_dates'], 'wb') as pk:
+    if not os.path.isfile(User.persist_dates):
+        with open(User.persist_dates, 'wb') as pk:
             dates = dict()
-            dates['start'] = User['csv_startDate']                                          # set start date for initial run, format: dd-mm-yyyy
+            dates['start'] = User.csv_startDate                                        # set start date for initial run, format: dd-mm-yyyy
             dates['end'] = (date.today() - timedelta(days=1)).strftime('%d-%m-%Y')          # set end date yesterday for initial run, format: dd-mm-yyyy
             dates['last_scrape'] = 'never'                                                  # flag for first run
             pickle.dump(dates, pk)
@@ -26,15 +26,15 @@ def initialize_dates_log():
 def create_dates_var():
     '''
     make dates log acessible
-    '''
-    with open(User['persist_dates'], 'rb') as pk:
-        dates = pickle.load(pk)
+    '''   
+    with open(User.persist_dates, 'rb') as pk:
+        dates = pickle.load(pk)  
     return dates
 
 def save_dates_loggingFile(dates):
     '''
-    export dates logging file
+    export dates logging file 
     '''
-    with open(User['persist_dates'], 'wb') as dpk:                                                                        # save logfile
+    with open(User.persist_dates, 'wb') as dpk:                                                                        # save logfile
         pickle.dump(dates, dpk)
     dpk.close()
