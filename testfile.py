7bf49a7de8700eb2678a2ff3cdf3fa340f2b6e4d
ckoplenig <tmpck@gmx.net>
2023-08-09 11:48:17 +0200

    modules - persistence, filehandling class setup

--- testfile.py
+++ testfile.py
@@ -1,44 +1,35 @@
 from modules.user import user
 
-# class print_user():
-#     def __init__(self) -> None:
-#         self = user()
-#         print(self.login_url)
-#         #return User
-        
-#     def print_name(self):
-#         #User = self.User
-#         print(self.username)
- 
-# # print_user()       
-# # print_user.print_name()
-
-# a = print_user()
-# a.print_name()
-
-# class Person():
-#     def __init__(self, vor, nach) -> None:
-#         self.vor = vor
-#         self.nach = nach
-        
-#     def pr_per(self):
-#         print("Vor:  %s, Nach:  %s" % (self.vor, self.nach)) 
-        
-# x = Person('ch', 'k')
-# x.pr_per()
-
 #### dependency injection
 class testUser():
-    def __init__(self, user_config) -> None:
-        # vars() creates dict, items() select key, value pairs       
-        for key, value in vars(user_config).items():
+    def __init__(self, UserClass: user) -> None:
+        """Initialize Class with all attributes from `UserClass`
+
+        Parameters
+        ----------
+        UserClass : class type
+            User data initiated via `user()` function from user module            
+        """
+        # vars() creates dict 
+        # items() iterate over key/value pairs       
+        for key, value in vars(UserClass).items():
             setattr(self, key, value)
     
     def ptu(self):
-        print(self.username)
+        print(self.login_url)
             
+    def __repr__(self) -> dict:
+        print(vars(self))
+        return self.username
 
-testUser(user()).ptu()
+#testUser(user()).ptu()
+
+a = user()
+#testUser(a).ptu()
+
+print(type(a))
+
+#print(testUser(a))
 #print_attributes(a)
 #attributes = vars(a)
 #print(attributes)
