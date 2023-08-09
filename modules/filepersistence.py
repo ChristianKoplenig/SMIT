1b7cb3ab9d5cfd984f77ac98fd2c42287d60f679
ckoplenig <tmpck@gmx.net>
2023-08-09 11:48:17 +0200

    modules - persistence, filehandling class setup

--- modules/filepersistence.py
+++ modules/filepersistence.py
@@ -1,49 +1,78 @@
 """
 Tools to persist data
 """
-import os
 import pickle
+from pathlib import Path
 from datetime import date, timedelta
-# Custom imports
-from modules.user import user
 
-User = user()
-
-def initialize_dates_log() -> None:
-    """Create log file for managing scraping dates.
-
-    If no persisted date exists, create dict for dates and use start date from User.
+class persistence():
+    """Class for dealing with all the data that needs to be peristed 
+    
+        Methods
+        -------
+        initialize_dates_log
+        create_dates_var
+        save_dates_loggingFile
     """
-    if not os.path.isfile(User.persist_dates):
-        with open(User.persist_dates, 'wb') as pk:
-            dates = dict()
-            dates['start'] = User.csv_startDate                                        # set start date for initial run, format: dd-mm-yyyy
-            dates['end'] = (date.today() - timedelta(days=1)).strftime('%d-%m-%Y')          # set end date yesterday for initial run, format: dd-mm-yyyy
-            dates['last_scrape'] = 'never'                                                  # flag for first run
-            pickle.dump(dates, pk)
-        pk.close()
+    def __init__(self, UserClass: 'modules.user.user') -> None:
+        """Initialize Class with all attributes from `UserClass`
 
-def create_dates_var() -> dict:
-    """Load dates log dict.
+        Parameters
+        ----------
+        UserClass : class type
+            User data initiated via `user()` function from user module            
+        """
+        # vars() creates dict 
+        # items() iterate over key/value pairs       
+        for key, value in vars(UserClass).items():
+            setattr(self, key, value)
+            
+    def initialize_dates_log(self) -> None:
+        """Create log file for managing scraping dates.
 
-    Returns
-    -------
-    dict
-        start, end and last scrape dates
-    """
+        If no persisted date exists, create dict for dates and use start date from user settings.
+        """
+        if not Path.is_file(self.persist_dates):
+            with open(self.persist_dates, 'wb') as pk:
+                dates = dict()
+                # Initial run setup
+                # Date format: dd-mm-yyyy
+                dates['start'] = self.csv_startDate 
+                dates['end'] = (date.today() - timedelta(days=1)).strftime('%d-%m-%Y')
+                dates['last_scrape'] = 'never'
+                pickle.dump(dates, pk)
+            pk.close()
 
-    with open(User.persist_dates, 'rb') as pk:
-        dates = pickle.load(pk)
-    return dates
+    def create_dates_var(self) -> dict:
+        """Load dates log dict.
+        
+        Loads the date logging variable from disc. 
 
-def save_dates_loggingFile(dates: dict):
-    """Persist dates in log dict.
+        Returns
+        -------
+        dict
+            [start], [end] and [last scrape] dates
+        """
+        with open(self.persist_dates, 'rb') as pk:
+            dates = pickle.load(pk)
+        return dates
 
-    Parameters
-    ----------
-    dates : dict
-        start, end and last scrape dates
-    """
-    with open(User.persist_dates, 'wb') as dpk:                                                                        # save logfile
-        pickle.dump(dates, dpk)
-    dpk.close()
+    def save_dates_loggingFile(self, dates: dict):
+        """Persist dates in log dict.
+        
+        Writes the dates logging variable to the disc.
+
+        Parameters
+        ----------
+        dates : dict
+            [start], [end], [last scrape]
+        """
+        with open(self.persist_dates, 'wb') as dpk:
+            pickle.dump(dates, dpk)
+        dpk.close()
+        
+    def __repr__(self) -> str:
+        return str(vars(self))
+        
+    def __str__(self) -> str:
+        return self.username
