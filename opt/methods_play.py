a6eee8bbbc3fa60c3701e8376d8dbff720d2701d
ckoplenig <tmpck@gmx.net>
2023-08-14 16:20:25 +0200

    opt - testfile for methods calling - initial commit

---
+++ opt/methods_play.py
@@ -0,0 +1,17 @@
+import sys
+import pathlib as pl
+
+# Add custom modules path to sys.path and import
+module_dir = pl.Path("__file__").resolve().parent
+
+if sys.path[0] != str(module_dir):
+    sys.path.insert(0, str(module_dir))
+
+from modules.user import user
+from modules.scrapedata import Webscraper
+#################################################################
+
+my_user = user()
+
+
+Webscraper(my_user).stromnetz_setup(my_user.csv_dl_daysum)
