d75e140bad36fb080caeaecc77eec615c69368ca
tmpck <tmpck@gmx.net>
2023-08-16 17:38:13 +0200

    rsa - load keys implemented

--- modules/rsahandling.py
+++ modules/rsahandling.py
@@ -19,8 +19,8 @@ class RsaTools():
         UserClass : user
         
         self.UserClass = UserClass
-        self.pub_key = pl.Path('./keys/public_key.pem')
-        self.priv_key = pl.Path('./keys/private_key.pem') 
+        self.pub_path = pl.Path('./keys/public_key.pem')
+        self.priv_path = pl.Path('./keys/private_key.pem') 
         #print(key_path)
         print('init')
     
@@ -28,13 +28,27 @@ class RsaTools():
         (public_key, private_key) = rsa.newkeys(1024)
         print('Keys generated')
 
-        with open(self.pub_key, 'wb') as key:
-            key.write(public_key.save_pkcs1('PEM'))
-            print('pub written')
-            
-        with open(self.priv_key, 'wb') as key:
-            key.write(private_key.save_pkcs1('PEM'))
-            print('priv written')
+        if not pl.Path(self.pub_path).exists():    
+            with open(self.pub_path, 'wb') as key:
+                key.write(public_key.save_pkcs1('PEM'))
+                print('pub written')
+        
+        if not pl.Path(self.priv_path).exists():    
+            with open(self.priv_path, 'wb') as key:
+                key.write(private_key.save_pkcs1('PEM'))
+                print('priv written')
 
+    def load_keys(self):
+        with open(self.pub_path, 'rb') as key:
+            public_key = rsa.PublicKey.load_pkcs1(key.read())
+            print('pub read')
+            
+        with open(self.priv_path, 'rb') as key:
+            private_key = rsa.PrivateKey.load_pkcs1(key.read())
+            print('priv read')        
+        return private_key, public_key
+        
+        
 a = user()
 RsaTools(a).generate_keys()
+print(RsaTools(a).load_keys())
