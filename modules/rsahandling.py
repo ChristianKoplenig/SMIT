6a1ca83386dd8bc09cb7c46c46b680d273bbb033
tmpck <tmpck@gmx.net>
2023-08-17 19:42:25 +0200

    rsa - moved file paths to user_settings.toml

--- modules/rsahandling.py
+++ modules/rsahandling.py
@@ -26,22 +26,19 @@ class RsaTools():
         UserInstance : 'user'
         
         self.user_instance = UserInstance
-        self.pub_path = pl.Path('./keys/public_key.pem')
-        self.priv_path = pl.Path('./keys/private_key.pem')
-        print('init')
+        self.pub_path = pl.Path(self.user_instance.public_key_path)
+        self.priv_path = pl.Path(self.user_instance.private_key_path)
         
         #If no key pair exists in keys folder generate one.
         (public_key, private_key) = rsa.newkeys(1024)
-        print('Keys generated') 
+        
         if not pl.Path(self.pub_path).exists():    
             with open(self.pub_path, 'wb') as key:
                 key.write(public_key.save_pkcs1('PEM'))
-                print('pub written')
 
         if not pl.Path(self.priv_path).exists():    
             with open(self.priv_path, 'wb') as key:
                 key.write(private_key.save_pkcs1('PEM'))
-                print('priv written')
 
     def load_keys(self) -> tuple[rsa.PrivateKey, rsa.PublicKey]:
         """Load keys from `keys` folder
@@ -53,11 +50,9 @@ class RsaTools():
         """
         with open(self.pub_path, 'rb') as key:
             public_key = rsa.PublicKey.load_pkcs1(key.read())
-            print('pub read')
             
         with open(self.priv_path, 'rb') as key:
             private_key = rsa.PrivateKey.load_pkcs1(key.read())
-            print('priv read')
         keys = {
             'private_key': private_key,
             'public_key': public_key
@@ -101,17 +96,4 @@ class RsaTools():
         return str(vars(self))
         
     def __str__(self) -> str:
-        return self.user_instance.username   
-    
-##################debug###############
-
-# if __name__ == '__main__':     
-#     a = user()
-#     #RsaTools(a).generate_keys()
-#     # print(RsaTools(a).load_keys())
-
-#     pass1 = 'teststring pass1'
-#     p1_encode = RsaTools(a).encrypt_pwd(pass1)
-#     print('enc: ' + str(p1_encode))
-#     #RsaTools(a).decrypt_pwd(p1_encode)
-#     print('dec: ' + str(RsaTools(a).decrypt_pwd(p1_encode)))
+        return self.user_instance.username
