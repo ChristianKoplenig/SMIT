11b16e4327aaf0ab11e7fa3c0e85263175d81b37
tmpck <tmpck@gmx.net>
2023-08-18 16:56:17 +0200

    rsa - load_keys() named tuple as return value

--- modules/rsahandling.py
+++ modules/rsahandling.py
@@ -1,6 +1,7 @@
 """
 Tools for dealing with cryptography
 """
+from collections import namedtuple
 import pathlib as pl
 import rsa
 
@@ -28,15 +29,17 @@ class RsaTools():
         self.user = user
         self.pub_path = pl.Path(self.user.public_key_path)
         self.priv_path = pl.Path(self.user.private_key_path)
+        no_public_key = not pl.Path(self.pub_path).exists()
+        no_private_key = not pl.Path(self.priv_path).exists()
         
         #If no key pair exists in keys folder generate one.
         (public_key, private_key) = rsa.newkeys(1024)
         
-        if not pl.Path(self.pub_path).exists():    
+        if no_public_key:    
             with open(self.pub_path, 'wb') as key:
                 key.write(public_key.save_pkcs1('PEM'))
 
-        if not pl.Path(self.priv_path).exists():    
+        if no_private_key:    
             with open(self.priv_path, 'wb') as key:
                 key.write(private_key.save_pkcs1('PEM'))
 
@@ -45,18 +48,18 @@ class RsaTools():
 
         Returns
         -------
-        dict
+        named tuple
             Private and public key
         """
+        Keys = namedtuple("Keys", ["public_key", "private_key"])
+        
         with open(self.pub_path, 'rb') as key:
             public_key = rsa.PublicKey.load_pkcs1(key.read())
             
         with open(self.priv_path, 'rb') as key:
             private_key = rsa.PrivateKey.load_pkcs1(key.read())
-        keys = {
-            'private_key': private_key,
-            'public_key': public_key
-        }        
+        
+        keys = Keys(public_key, private_key)
         return keys
         
     def encrypt_pwd(self, pwd: str) -> bytes:
@@ -73,7 +76,7 @@ class RsaTools():
             Encrypted input.
         """
         pwd_enc = pwd.encode('utf8')
-        pwd_crypt = rsa.encrypt(pwd_enc, self.load_keys()['public_key'])
+        pwd_crypt = rsa.encrypt(pwd_enc, self.load_keys().public_key)
         return pwd_crypt
     
     def decrypt_pwd(self, pwd: bytes) -> str:
@@ -89,7 +92,7 @@ class RsaTools():
         str
             Decrypted input.
         """
-        pwd_decrypt = rsa.decrypt(pwd, self.load_keys()['private_key'])
+        pwd_decrypt = rsa.decrypt(pwd, self.load_keys().private_key)
         return pwd_decrypt.decode('utf8')
         
     def __repr__(self) -> str:
