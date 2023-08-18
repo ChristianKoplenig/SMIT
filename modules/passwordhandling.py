8b853e71ddaa41d2d0e82091bb3fdb3968cc5b53
tmpck <tmpck@gmx.net>
2023-08-18 17:31:53 +0200

    pwd - window with checkbutton implemented

--- modules/passwordhandling.py
+++ modules/passwordhandling.py
@@ -1,23 +1,34 @@
+"""
+Tools for handling the password input/save dialog
+"""
 import tkinter as tk
 from tkinter import ttk
 
-root = tk.Tk()
-root.title('Enter Password')
-root.geometry('300x200')
-
-checkbox_value = tk.StringVar()
-
-def checkbox_changed():
-    """Do something when the checkbox is checked.
+class UiTools():
+    """Password input and save dialog
     """
-    ch =  checkbox_value.get()
-    print(ch)
 
-ttk.Checkbutton(root,
-                text= 'save password',
-                command= checkbox_changed,
-                onvalue='yes',
-                offvalue='no',
-                variable= checkbox_value).pack()
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.root.title('Enter Password')
+        self.root.geometry('300x200')
+        self.checkbox_value = tk.StringVar()
+        #self.root.mainloop()
 
-root.mainloop()
+    def checkbox_changed(self):
+        """Do something when the checkbox is checked.
+        """
+        ch =  self.checkbox_value.get()
+        print(ch)
+    
+    def pwd_dialog(self):
+        ttk.Checkbutton(self.root,
+                        text= 'save password',
+                        command= self.checkbox_changed,
+                        onvalue='yes',
+                        offvalue='no',
+                        variable= self.checkbox_value).pack()
+        self.root.mainloop()
+    
+    
+UiTools().pwd_dialog()
