afbe90aae439fd6c20e9f0cf3cd559679fce22b9
tmpck <tmpck@gmx.net>
2023-08-19 15:44:57 +0200

    pwd - core functions implemented

--- modules/passwordhandling.py
+++ modules/passwordhandling.py
@@ -9,26 +9,63 @@ class UiTools():
     """
 
     def __init__(self) -> None:
-        self.root = tk.Tk()
-        self.root.title('Enter Password')
-        self.root.geometry('300x200')
+        
+        # create window
+        self.window = tk.Tk()
+        self.window.title('Password Dialog')
+        self.window.geometry('300x200')
+        
         self.checkbox_value = tk.StringVar()
-        #self.root.mainloop()
+    
+    def tk_text(self, txt_input):
+        """Generate text widget
+        """
+        text = tk.Label(
+            text= txt_input,
+            height=3)
+        text.pack()
+        
+    def tk_button(self):
+        """Generate button
+        """
+        button = tk.Button(
+            text='Confirm'
+        )
+        button.pack()    
 
-    def checkbox_changed(self):
+    def tk_checkbox(self):
         """Do something when the checkbox is checked.
         """
         ch =  self.checkbox_value.get()
         print(ch)
+        
+    def tk_entry(self):
+        """Generate text entry field for password input.
+        """
+        entry = tk.Entry(
+            fg='yellow',
+            bg='blue',
+            width=15
+        )
+        entry.pack()
     
     def pwd_dialog(self):
-        ttk.Checkbutton(self.root,
+        """Initiate enter password dialog
+        """
+        self.tk_text('Please enter Password')
+        #self.tk_entry()
+        pwd = tk.Entry(fg='yellow', bg='blue', width=15)
+        pwd.pack()
+        print(pwd.get())
+        ttk.Checkbutton(self.window,
                         text= 'save password',
-                        command= self.checkbox_changed,
+                        command= self.tk_checkbox,
                         onvalue='yes',
                         offvalue='no',
                         variable= self.checkbox_value).pack()
-        self.root.mainloop()
+        self.tk_button()
+        self.tk_text('See disclaimer for information on password storage')
+        self.window.mainloop()
     
     
 UiTools().pwd_dialog()
