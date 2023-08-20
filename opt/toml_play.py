0fcece1f58410232662eb200709fcaef219d97d3
tmpck <tmpck@gmx.net>
2023-08-20 18:53:08 +0200

    opt - toml add pwd works

--- opt/toml_play.py
+++ opt/toml_play.py
@@ -1,5 +1,6 @@
 import sys
 import pathlib as pl
+import tomlkit
 
 # Add custom modules path to sys.path and import
 module_dir = pl.Path("__file__").resolve().parent
@@ -9,67 +10,90 @@ if sys.path[0] != str(module_dir):
 
 from modules.user import user
 
-# Toml imports
-import tomlkit
-if sys.version_info < (3, 11):
-    import tomli as tomlib
-else:
-    import tomlib
-
-#print(tomlib.__version__)
-####### toml conda versions
-# toml                      0.10.2             pyhd3eb1b0_0  
-# tomli                     2.0.1            py39h06a4308_0  
+#print(tomlkit.__version__)
+#########################
 # tomlkit                   0.11.1           py39h06a4308_0 
 #########################
 
-class TomlPlay():
-    def __init__(self, user: user) -> None:
-        self.user = user
-        self.user_data = pl.Path(self.user.user_data_path)
-        
-    def load_toml(self):
-        with open(self.user_data, mode='rb') as toml:
-            data = tomlib.load(toml)
-            print('type: ' + str(type(data)))
-            print(data)
-            print(data['Login']['username'])
-            print(data['Login'])
-
-a = user()            
-#TomlPlay(a).load_toml()
-
 class TomlTools():
+    """Class for handling toml files
+        Attributes
+        ----------
+        user : class instance
+            Holds user information      
+        
+        Methods
+        -------
+        load_toml_file():
+            Return toml object from filesystem.
+        save_toml_file():
+            Write toml object to filesystem. 
+    
+    """
     def __init__(self, user: user) -> None:
         self.user = user
         self.user_data = pl.Path(self.user.user_data_path)
         
-    def load_tomlkit(self):
-        """Loads `user_data.toml` as python toml object
+    def load_toml_file(self, filename: pl.Path) -> tomlkit:
+        """Read `filename` file and return TOML object.
+
+        Parameters
+        ----------
+        filename : pl.Path
+            Path object to the `.toml` file.
+
+        Returns
+        -------
+        object
+            TOML object
         """
-        with open(self.user_data, mode='rt', encoding='utf-8') as ud:
-            data = tomlkit.load(ud)
+        with open(filename, mode='rt', encoding='utf-8') as file:
+            data = tomlkit.load(file)
             print('type: ' + str(type(data)))
             print(data['Login']['username'])
         
         return data    
     
-    def save_tomlkit(self, config_file):
-        """Write 'user_data.toml`
-        """
-        with open(self.user_data, mode='wt', encoding='utf-8') as ud:
-            tomlkit.dump(config_file, ud)
+    def save_toml_file(self, filename: pl.Path, toml_object: tomlkit) -> None:
+        """Takes python toml object and writes `.toml` file to filesystem
 
-ui_data = TomlTools(a).load_tomlkit()
+        Parameters
+        ----------
+        filename : pl.Path
+            Path to output file on filesystem
+        toml_object : tomlkit
+            Python TOML object
+        """
+        with open(filename, mode='wt', encoding='utf-8') as file:
+            tomlkit.dump(toml_object, file)
+
+    def toml_append_password(self, toml_object: tomlkit, pwd: str) -> None:
+        """Append password entry und Login table.
+
+        Parameters
+        ----------
+        toml_object : tomlkit
+            Python TOML object.
+        pwd : str
+            Password for webscraping login
+        """
+        toml_object['Login'].add("password", pwd) # pylint: disable=no-member
+        toml_object['Login']['password'].comment('Input from Password Dialog')
+######## run ##################
+a = user()
+toml_filename = pl.Path(a.user_data_path)
+
+ui_data = TomlTools(a).load_toml_file(toml_filename)
 print('ui_data: ' + str(ui_data))
 print('before: ' + str(ui_data['Login']))
 
-pwd = "as /n df  \n bla"
-ui_data['Login'].add("password", pwd) # pylint: disable=no-member
-ui_data['Login']['password'].comment('Automatic add')
+pwd = "pwd variable"
+# ui_data['Login'].add("password", pwd) # pylint: disable=no-member
+# ui_data['Login']['password'].comment('Automatic add')
 
+TomlTools(a).toml_append_password(ui_data, pwd)
 print('after: ' + str(ui_data['Login']))
 
-TomlTools(a).save_tomlkit(ui_data)
+TomlTools(a).save_toml_file(toml_filename, ui_data)
 
 print(ui_data['Login']['password'])
