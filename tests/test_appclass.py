8645e69ec84ed89fed6749f26314faeea6540dd0
tmpck <tmpck@gmx.net>
2023-09-01 15:34:03 +0200

    tests - markers implemented, appclass attribute tests implemented

--- tests/test_appclass.py
+++ tests/test_appclass.py
@@ -1,6 +1,18 @@
+# pylint: disable=no-member
+import pytest # pylint: disable=import-error
 from SMIT.application import Application
 
-def test_create_app():
+@pytest.mark.application
+def test_application_username():
+    """Test if the app.Login['username'] variable has any attribute.
+    """
     app = Application()
     assert app.Login['username'] != ''
+
+@pytest.mark.application
+def test_application_module():
+    """Test if the app.gui variable has any attribute.
+    """
+    app = Application()
+    assert app.gui != ''
     
