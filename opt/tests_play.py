fcbb1e08282a53b8160975b56eff7b2fc6ee3402
tmpck <tmpck@gmx.net>
2023-09-05 15:36:45 +0200

    tests - application implemented

---
+++ opt/tests_play.py
@@ -0,0 +1,19 @@
+# pylint: disable=no-member
+# pylint: disable=import-outside-toplevel
+import pathlib as pl
+
+# Setup
+from SMIT.application import Application
+app = Application(True)
+################################################
+# pylint: disable=no-member 
+# folders = [] 
+# for folder, folder_path in app.Folder.items():
+#     print(f'Folder: {folder}')
+#     print(f'Folder Path: {folder_path}')
+    
+# for folder_path in app.Folder.values():
+#     print(folder_path)
+modules = app._load_modules()
+for key, value in modules.items():
+    print(f'Key: {key} - Value: {value}')
