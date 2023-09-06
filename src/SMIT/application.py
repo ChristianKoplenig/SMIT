bb9b3c47e635309e1884c2870c8e2959e13feb45
tmpck <tmpck@gmx.net>
2023-09-06 10:24:41 +0200

    code cleaning

--- src/SMIT/application.py
+++ src/SMIT/application.py
@@ -11,20 +11,21 @@ from SMIT.rsahandling import RsaTools
 from SMIT.filehandling import OsInterface, TomlTools
 from SMIT.userinput import UiTools
 
+
 class Application:
     # pylint: disable=no-member
     """Main class for application setup.
-    
+
     Load user configuration files.
-    Initialize the folder structure.   
+    Initialize the folder structure.
     Instantiate all custom modules.
     Set/store password according to user preference.
     """
-    def __init__(self, dummy: bool=False) -> None:
-        
+    def __init__(self, dummy: bool = False) -> None:
+
         # Attribute needed for scrape and move routine
         self.dummy = dummy
-        
+
         if self.dummy is False:
             # Load paths to user configuration files
             self.user_data = pl.Path('config/user_data.toml')
@@ -32,32 +33,32 @@ class Application:
         else:
             # Load application with dummy configuration
             self._setup_dummy_user()
-        
-        self._add_TOML_to_attributes(self.user_data)    
+
+        self._add_TOML_to_attributes(self.user_data)
         self._add_TOML_to_attributes(self.user_settings)
         self._initialize_folder_structure()
         self._setup_logger()
         self._add_modules_to_attributes()
-        
+
         if dummy is False:
             # Load Gui Dialog
             self.gui.credentials_dialog()
-        
+
         self.logger.info('Application with user "%s" instantiated', self.Login["username"])
-    
+
     def _add_modules_to_attributes(self) -> None:
         """Read modules dict and assign it to self.
-        
+
         Calls function _load_modules()
         In modules dict the custom modules are stored as key, value pairs.
-        Loading the modules dict makes the custom methods easy accessible. 
-        """ 
+        Loading the modules dict makes the custom methods easy accessible.
+        """
         for key, value in self._load_modules().items():
             setattr(self, key, value)
-            
+
     def _add_TOML_to_attributes(self, file_path: pl.Path) -> None:
         """Read config file and assign parameters to self.
-        
+
         Call `tomlkit` library and read parameters from a `.toml` file.
         Loop trough config file and assign parameters to self.
 
@@ -75,17 +76,17 @@ class Application:
 
     def _initialize_folder_structure(self) -> None:
         """Create folder structure.
-        
+
         If the needed folders do not exist they will be created.
         If the folders exist no error will be raised
         """
-        # pylint: disable=no-member  
+        # pylint: disable=no-member
         for folder_path in self.Folder.values():
-            os.makedirs(folder_path, exist_ok= True) 
-        
+            os.makedirs(folder_path, exist_ok=True)
+
     def _load_modules(self) -> dict:
         """Create a dict with all loaded modules.
-        
+
         Assign trivial names to instantiated modules.
         """
         modules = dict([
@@ -94,16 +95,16 @@ class Application:
             ('toml_tools', TomlTools(self)),
             ('os_tools', OsInterface(self)),
             ('persistence', Persistence(self)),
-            ('scrape', Webscraper(self))          
+            ('scrape', Webscraper(self))
         ])
         return modules
-    
+
     def _setup_logger(self):
         """Configuration for logging
         """
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
-        
+
         formatter = logging.Formatter('%(asctime)s :: %(levelname)-8s :: [%(module)s:%(lineno)d] :: %(message)s')
 
         file_handler = logging.FileHandler(self.Path['log_file'])
@@ -115,7 +116,7 @@ class Application:
         console_handler.setLevel(logging.DEBUG)
         console_handler.setFormatter(formatter)
         self.logger.addHandler(console_handler)
-        
+
     def _setup_dummy_user(self):
         """Create environment for testing purposes
         """
@@ -123,17 +124,16 @@ class Application:
         dummy_data_folder = pl.Path('./.dummy').absolute()
         if dummy_data_folder.exists():
             shutil.rmtree(dummy_data_folder)
-                    
-        
+
         # Set paths for copying dummy files
         source_dummy_csv = pl.Path('./opt/dummy_user/').absolute()
         dest_dummy_csv = pl.Path('./.dummy/csv_raw/daily').absolute()
         dest_dummy_settings = pl.Path('./.dummy/config').absolute()
-        
+
         # Create folder for dummy raw files
         dest_dummy_csv.mkdir(parents=True, exist_ok=True)
         dest_dummy_settings.mkdir(parents=True, exist_ok=True)
-        
+
         # Copy csv files
         for filename in source_dummy_csv.glob('*.csv'):
             dest = dest_dummy_csv / filename.name
@@ -142,10 +142,10 @@ class Application:
         for filename in source_dummy_csv.glob('*.toml'):
             dest = dest_dummy_settings / filename.name
             shutil.copy2(filename, dest)
-                      
+
         # Set paths to dummy configuration
         self.user_data = pl.Path('./.dummy/config/dummy_data.toml')
         self.user_settings = pl.Path('./.dummy/config/dummy_settings.toml')
-        
+
     def __repr__(self) -> str:
         return f"Module '{self.__class__.__module__}.{self.__class__.__name__}'"
