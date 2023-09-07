b530d2830c55b78ceef1c7626da8a7e6a49cb7ac
tmpck <tmpck@gmx.net>
2023-09-07 15:35:27 +0200

    doc - application done

--- src/SMIT/application.py
+++ src/SMIT/application.py
@@ -30,23 +30,31 @@ class Application:
     # pylint: disable=no-member
     """Create application framework.
 
-    - Set paths variables to config files.
-    - Read user setting from config files and assign to `self`.
+    User
+    ----
+    - Set paths to user configuration files.
+    - Assign user settings from config files to the application instance.
     - On initial run create folder structure according to `./config/user_settings.toml`.
-    - Initialize logging function according to `SMIT.application.Application._setup_logger`.
-    - Instantiate modules and assign to `self`.
+    - Configure logging function.
+    - Instantiate modules and assign to application instance.
+    - Call user credentials GUI.
 
     Dummy User
     ----------
+    Use for demonstration and testing.  
+    
+    - Run application with local data. 
+    - No scraping functionality.
+    - No call of tkinter module -> No GUI is loaded.
     - On each instantiation delete and newly create `./.dummy` folder.
-    - Locally simulate application features without scraping.
-    - No call of tkinter module - GUI
+    - Data source: `./opt/dummy_user`.
+    - Working directory: `./dummy`. 
 
     Parameters
     ----------
 
     dummy : bool = False
-        Flag for running with dummy user
+        Flag for activating dummy user.
     """
     def __init__(self, dummy: bool = False) -> None:
 
@@ -76,9 +84,9 @@ class Application:
     def _add_modules_to_attributes(self) -> None:
         """Read modules dict and assign it to self.
 
-        Calls function _load_modules()
-        In modules dict the custom modules are stored as key, value pairs.
-        Loading the modules dict makes the custom methods easy accessible.
+        Calls function `SMIT.application.Application._load_modules()`  
+        Iterates over key, value pairs in modules dict 
+        and assigns the module instances to the application instance.  
         """
         for key, value in self._load_modules().items():
             setattr(self, key, value)
@@ -86,12 +94,12 @@ class Application:
     def _add_TOML_to_attributes(self, file_path: pl.Path) -> None:
         """Read config file and assign parameters to self.
 
-        Call `tomlkit` library and read parameters from a `.toml` file.
-        Loop trough config file and assign parameters to self.
+        Call `tomlkit` library and read parameters from a `.toml` file.  
+        Loop trough the file and assign all parameters to the application instance.
 
         Parameters
         ----------
-        file_path : pure path object
+        file_path : pathlib.Path
             Path to `.toml` config file.
         """
         # load file
@@ -104,17 +112,30 @@ class Application:
     def _initialize_folder_structure(self) -> None:
         """Create folder structure.
 
-        If the needed folders do not exist they will be created.
-        If the folders exist no error will be raised
+        Iterate over the Folder table in settings file.  
+        If a folder does not exist it will be created.    
+        If the folders exists no error will be raised.  
         """
         # pylint: disable=no-member
         for folder_path in self.Folder.values():
             os.makedirs(folder_path, exist_ok=True)
 
     def _load_modules(self) -> dict:
-        """Create a dict with all loaded modules.
-
-        Assign trivial names to instantiated modules.
+        """Create dict with all modules.
+        
+        This method exists to assure that each module
+        is instatiated just once. Additionally the 
+        dicitionary is used in the test setup to check if
+        all modules are loaded.        
+        
+        Instantiate all custom modules.  
+        Assign trivial names to instantiated modules via keys.  
+        
+        Returns
+        -------
+        dict
+            Dictionairy where keys are trivial names and values
+            are intantiated modules objects.
         """
         modules = dict([
             ('gui', UiTools(self)),
@@ -126,8 +147,20 @@ class Application:
         ])
         return modules
 
-    def _setup_logger(self):
-        """Configuration for logging
+    def _setup_logger(self) -> None:
+        """Configuration for logging.
+        
+        The default log folder is `./log` and the logfile is called `app.log`.  
+        Setting logging levels for the log file is done via `file_handler.setLevel()`.  
+        Setting logging levels for terminal output is done via `console_handler.setLevel()`.    
+        
+        Logging Levels:
+        ---------------
+        - DEBUG
+        - INFO
+        - WARNING
+        - ERROR
+        - CRITICAL
         """
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
@@ -140,14 +173,23 @@ class Application:
         self.logger.addHandler(file_handler)
 
         console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.DEBUG)
+        console_handler.setLevel(logging.INFO)
         console_handler.setFormatter(formatter)
         self.logger.addHandler(console_handler)
 
     def _setup_dummy_user(self):
-        """Create environment for testing purposes
+        """Create environment for testing purposes.
+        
+        This user will not run any scraping routines
+        or GUI dialogs. It is intended to use as demonstration
+        user and for unit testing.
+        
+        - On each call delete `.dummy` folder.
+        - Recreate folder and populate with clean data.
+        - Source for dummy data is `./opt/dummy_user`.
+        - Set config path to `./.dummy/config`
         """
-        # Reset dummy user on login
+        # Reset .dummy folder
         dummy_data_folder = pl.Path('./.dummy').absolute()
         if dummy_data_folder.exists():
             shutil.rmtree(dummy_data_folder)
@@ -157,7 +199,8 @@ class Application:
         dest_dummy_csv = pl.Path('./.dummy/csv_raw/daily').absolute()
         dest_dummy_settings = pl.Path('./.dummy/config').absolute()
 
-        # Create folder for dummy raw files
+        # Create folders which are needed before user init 
+        # Rest will be created with Application.__init__
         dest_dummy_csv.mkdir(parents=True, exist_ok=True)
         dest_dummy_settings.mkdir(parents=True, exist_ok=True)
 
