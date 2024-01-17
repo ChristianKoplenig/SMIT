8f8f35dd56b0b8cda593abb12b353b9fbe9e4508
Christian Koplenig <tmpck@gmx.net>
2024-01-17 16:54:49 +0100

    db - Field validation integrated in AuthDbSchema model

--- src/db/auth_schema.py
+++ src/db/auth_schema.py
@@ -1,10 +1,11 @@
 from typing import Optional
 from datetime import datetime
+import re
 
 from sqlmodel import Field, SQLModel
 from typing_extensions import Annotated, Doc
 
-from pydantic import StringConstraints
+from pydantic import StringConstraints, ValidationInfo, field_validator
 
 class AuthDbSchema(SQLModel, table=True):
     """
@@ -12,8 +13,8 @@ class AuthDbSchema(SQLModel, table=True):
 
     Attributes:
         id (Optional[int]): The unique identifier of the user.
-        username (str): Smit Application username.
-        password (str): Smit Application password.
+        username (str): Authentication username.
+        password (str): Authentication password.
         email (str, optional): The email address of the user.
         sng_username (str, optional): The username for energy provider login.
         sng_password (str, optional): The password for energy provider login.
@@ -31,13 +32,13 @@ class AuthDbSchema(SQLModel, table=True):
                         StringConstraints(strip_whitespace=True, 
                             to_lower=True, 
                             pattern=r'^[A-Za-z0-9_]+$'),
-                        Doc("Smit application username."),
+                        Doc("Authentication username."),
                         ] = Field(index=True,
-                                  description="Smit application username.",
+                                  description="Authentication username.",
                                   unique=True,
                                   min_length=3)    
     
-    password: str = Field(..., description=" Hash of Smit application password")
+    password: str = Field(description="Hash of Authentication password")
     
     # Additional fields
     email: Optional[str] = Field(default=None, description="Mail address for pwd recovery")
@@ -46,5 +47,32 @@ class AuthDbSchema(SQLModel, table=True):
     sng_password: Optional[str] = Field(default=None, description="Elictricity provider password")
     daymeter: Optional[int] = Field(default=None, description="Day meter endpoint number")
     nightmeter: Optional[int] = Field(default=None, description="Night meter endpoint number")
+    
+    # Validation
+    @field_validator('username', 'sng_username')
+    @classmethod
+    def validate_usernames(cls, v: str, info: ValidationInfo) -> str:
+        if len(v) < 4:
+            raise ValueError(f"{info.field_name} must be at least 5 characters long")
+        return v
+
+    @field_validator('email')
+    @classmethod
+    def validate_email(cls, v: str) -> str:
+        pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        if not re.match(pattern, v):
+            raise ValueError("Invalid email address")   
+
+    @field_validator('daymeter', 'nightmeter')
+    @classmethod
+    def validate_meter(cls, v: str) -> str:
+        if len(str(v)) != 6:
+            raise ValueError("Meter number must be 6 characters long")
+    
+    
+    
+    
+    
+    
     
     
--- src/st_auth/auth_api.py
+++ src/st_auth/auth_api.py
@@ -1,97 +1,27 @@
-import re
-from matplotlib.pyplot import cla
-from pydantic import BaseModel, field_validator
-
-# Schema for data validation
-from db.auth_schema import AuthDbSchema
 # Database connection
 from db.smitdb import SmitDb
+# Database schema
+from db.auth_schema import AuthDbSchema
 
-
-
-
-class AuthApiSchema(BaseModel):
-    """
-    Schema for user management.
-
-    Attributes:
-        users (pydantic.Model): Database schema for authentication data.
-        cookie_name: str
-            The name of the JWT cookie stored on the client's browser for passwordless reauthentication.
-        key: str
-            The key to be used for hashing the signature of the JWT cookie.
-        cookie_expiry_days: float
-            The number of days before the cookie expires on the client's browser.
-        preauthorized: list
-            The list of emails of unregistered users authorized to register.
-        validator: Validator
-            A Validator object that checks the validity of the username, name, and email fields.
-    """
-    db_schema: AuthDbSchema
-    cookie_name: str = 'streamlit-smit-app'
-    key: str = 'cookey'
-    cookie_expiry_days: float = 30
-    preauthorized: list = []
-
-    @field_validator('db_schema')
-    @classmethod
-    def validate_auth_table(cls, v: str) -> str:
-        """
-        Validates the authentication table.
-        Args:
-            v (str): The authentication table to be validated.
-        Raises:
-            ValueError: If the username is less than 3 characters long.
-            ValueError: If the email address is invalid.
-            ValueError: If the meter number is not 6 characters long.
-        Returns:
-            str: The validated authentication table.
-        """
-        # Smit username
-        smit_usr = v.username
-        sng_usr = v.sng_username
-        if len(smit_usr) < 3 or len(sng_usr) < 3:
-            raise ValueError("Username must be at least 3 characters long")
-        
-        # Standard email address validation
-        pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-        if not re.match(pattern, v.email):
-            raise ValueError("Invalid email address")   
-        
-        # Additional validation for use case specific fields
-        # Length of meter number
-        if len(str(v.daymeter)) != 6 or len(str(v.nightmeter)) != 6:
-            raise ValueError("Meter number must be 6 characters long")
-        return v
-        
-    @field_validator('preauthorized')
-    @classmethod
-    def validate_preauthorized(cls, v: str) -> str:
-        """
-        Validates the preauthorized list.
-
-        Args:
-            v (str): The preauthorized list to be validated.
-
-        Raises:
-            ValueError: If the email address is invalid.
-
-        Returns:
-            str: The validated preauthorized list.
-        """
-        for email in v:
-            pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-            if not re.match(pattern, email):
-                raise ValueError("Invalid email address in preauthorized list") 
-        return v
-
-######### Provide data from db #########
 class AuthModel:
     """
-    This class provides methods for creating user dictionaries and models.
+    Class for retrieving data from the authentication table.
+    
+    Methods:
+        create_users_dict(db_all: list) -> dict: 
+            Create a dictionary of users with their attributes.
+        create_user_models(users: dict) -> dict: 
+            Return dictionary with validated user models from AuthDbSchema.
+        single_user_model(user: list) -> dict[str, str]: 
+            Return unvalidated dictionary with single user data from authentication table.
+        get_user(username: str) -> dict[str, any]: 
+            Retrieves a user from the database based on the provided username.
     """
-
-    def create_users_dict(self, db_all) -> dict:
+    def __init__(self):
+        self.db_connection = SmitDb(AuthDbSchema)
+        
+        
+    def create_users_dict(self, db_all: list) -> dict:
         """
         Create a dictionary of users with their attributes.
 
@@ -135,20 +65,38 @@ class AuthModel:
                 models[uid['id']] = AuthDbSchema.model_validate(uid)
         return models
 
-    def single_user_model(self, user: list) -> dict:
+    def single_user_model(self, user: list) -> dict[str, str]:
         """
-        Return dictionary with validated single user data from AuthDbSchema.
+        Return unvalidated dictionary with single user data from authentication table.
 
         Args:
             user (list): Query result for single user from auth table.
 
         Returns:
-            dict: A dictionary containing the user model.
+            dict: A dictionary containing the user attributes as key, value pairs.
         """
         model: dict = {}
         model = AuthDbSchema.model_dump(user)
         return model
 
+
+    # Get validated dict from db
+    def get_user(self, username: str) -> dict[str, any]:
+        """
+        Retrieves a user from the database based on the provided username.
+
+        Args:
+            username (str): The username of the user to retrieve.
+
+        Returns:
+            dict[str, any]: A dictionary representing the user's data.
+        """
+        row_db: tuple = self.db_connection.select_username(username)
+        model_db: dict = self.single_user_model(row_db)
+        
+        validated_schema: AuthDbSchema = AuthDbSchema().model_validate(model_db)
+        
+        return validated_schema.model_dump()
 
 ######## work with all users ######## 
 
@@ -165,8 +113,17 @@ class AuthModel:
 
 #######
 ####### single row ########
+
+
 # db_connection = SmitDb(AuthDbSchema)
 # user_row = db_connection.select_username('dummy_user')
+
+# model_val = AuthModel().get_user('aaa00')
+
+# print('---- model outside ----')
+# print(model_val['username'])
+
+
 # user_model = single_user_model(user_row)
 
 # # print('-----User row-----')
