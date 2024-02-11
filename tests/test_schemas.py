5636f40d0f928fc8ecb1f7e5d0034e5bac909bc2
Christian Koplenig <tmpck@gmx.net>
2024-02-11 15:56:47 +0100

    tests - added unique constraint tests to schema validation

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,20 +1,21 @@
-from multiprocessing import dummy
+
 import pytest
-from typing import Any, TYPE_CHECKING
+from typing import TYPE_CHECKING
+
+from sqlalchemy.exc import IntegrityError
 
 from tests.conftest import TestSmitDb
-# from src.db.schemas import AuthenticationSchema
 
-# if TYPE_CHECKING:
+if TYPE_CHECKING:
 # from .conftest import TestSmitDb
-# from sqlmodel import Session
+    from sqlmodel import SQLModel
 
 
 @pytest.mark.smoke
 @pytest.mark.schema
 def test_invalid_users(
     invalid_users: dict[str, dict[str, str]], db_instance_empty: TestSmitDb
-):
+) -> None:
     for user in invalid_users:
         with pytest.raises(ValueError):
             db_instance_empty.db_schema.model_validate(invalid_users[user])
@@ -24,6 +25,22 @@ def test_invalid_users(
 @pytest.mark.schema
 def test_valid_users(
     valid_users: dict[str, dict[str, str]], db_instance_empty: TestSmitDb
-):
+) -> None:
     for user in valid_users:
         db_instance_empty.db_schema.model_validate(valid_users[user])
+
+@pytest.mark.smoke
+@pytest.mark.schema
+def test_unique_constraint(
+    valid_users: dict[str, dict[str, str]],
+    db_instance_empty: TestSmitDb,
+    session,
+) -> None:
+    user1: SQLModel = db_instance_empty.db_schema.model_validate(valid_users["dummy_user"])
+    user2: SQLModel = db_instance_empty.db_schema.model_validate(valid_users["dummy_user"])
+
+    with session:
+        with pytest.raises(IntegrityError):
+            session.add(user1)
+            session.add(user2)
+            session.commit()
